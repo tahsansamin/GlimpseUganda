c9bba78df9cff559bc983425482b082f5d089abb
tahsansamin <tahsansamin@gmail.com>
2026-05-28 23:23:49 +0300

    moved from local url to railway url that frontend connects too

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:8000'
+  baseURL: 'https://glimpseuganda-production.up.railway.app'
 });
 
 export default apiClient;

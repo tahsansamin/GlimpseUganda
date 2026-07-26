a1853ae326a0f8913a28be035b53215456dd0786
tahsansamin <tahsansamin@gmail.com>
2026-07-26 21:50:01 +0600

    remove commented-out function

--- backend/app_main.py
+++ backend/app_main.py
@@ -128,33 +128,6 @@ def setup_namespaces():
     return setup_pinecone_namespaces()
 # setup_namespaces()
 
-# def rate_limited(max_calls: int, time_frame: int):
-#     """
-#     :param max_calls: Maximum number of calls allowed in the specified time frame.
-#     :param time_frame: The time frame (in seconds) for which the limit applies.
-#     :return: Decorator function.
-#     """
-#     def decorator(func):
-#         calls = []
-
-#         @wraps(func)
-#         async def wrapper(request: Request, *args, **kwargs):
-
-#             now = time.time()
-#             calls_in_time_frame = [call for call in calls if call > now - time_frame]
-            
-#             if len(calls_in_time_frame) >= max_calls:
-#                 raise HTTPException(
-#                     status_code=status.HTTP_429_TOO_MANY_REQUESTS, 
-#                     detail="Rate limit exceeded."
-#                 )
-                
-#             calls.append(now)
-#             return await func(request, *args, **kwargs)
-
-#         return wrapper
-
-#     return decorator
 
 def ingest_all_city_documents_to_pinecone():
 
